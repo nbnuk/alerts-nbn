ea6ba8cb711fb2a6b7a978f424a7d823bc5d8d72
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-03-23 19:08:40 +0000

    Bootstrap

--- grails-app/init/alerts/nbn/BootStrap.groovy
+++ grails-app/init/alerts/nbn/BootStrap.groovy
@@ -1,9 +1,40 @@
-package alerts.nbn
+package uk.org.nbn.alerts
 
-class BootStrap {
+import au.org.ala.alerts.Query
+import au.org.ala.alerts.PropertyPath
+
+class BootStrap extends au.org.ala.alerts.BootStrap{
+
 
     def init = { servletContext ->
+        log.info("Running nbn bootstrap queries.")
+        preloadQueries()
+        log.info("Done nbn bootstrap queries.")
+        super.preloadQueries()
     }
-    def destroy = {
+
+    private void preloadQueries() {
+        if(Query.findAllByName('Annotations').isEmpty()){
+            Query newAssertions = (new Query([
+                    baseUrl: grailsApplication.config.biocacheService.baseURL,
+                    baseUrlForUI: grailsApplication.config.biocache.baseURL,
+                    resourceName:  grailsApplication.config.postie.defaultResourceName,
+                    name: 'Annotations',
+                    updateMessage: 'annotations.update.message',
+                    description: 'Notify me when annotations are made on any record.',
+                    queryPath: '/occurrences/search?fq=user_assertions:*&q=user_assertions:true AND last_assertion_date:[___DATEPARAM___%20TO%20*]&sort=last_assertion_date&dir=desc&pageSize=20&facets=basis_of_record',
+                    queryPathForUI: '/occurrences/search?fq=user_assertions:*&q=last_assertion_date:[___DATEPARAM___%20TO%20*]&sort=last_assertion_date&dir=desc',
+                    dateFormat: """yyyy-MM-dd'T'HH:mm:ss'Z'""",
+                    emailTemplate: '/email/biocache',
+                    recordJsonPath: '\$.occurrences[*]',
+                    idJsonPath: 'uuid'
+            ])).save()
+            new PropertyPath([name: "totalRecords", jsonPath: "totalRecords", query: newAssertions, fireWhenNotZero: true]).save()
+            new PropertyPath([name: "last_assertion_record", jsonPath: "occurrences[0].rowKey", query: newAssertions]).save()
+        }
+
+        log.info("end of preloadQueries")
     }
+
+    def destroy = {}
 }
