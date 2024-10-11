49edae1082dc359c53c501b8b50c2e1db844cfc3
Helen Manders-Jones <h.manders.jones@nbn.org.uk>
2024-10-11 13:21:23 +0100

    first commit - just a guide

--- grails-app/controllers/uk/org/nbn/alerts/UrlMappings.groovy
+++ grails-app/controllers/uk/org/nbn/alerts/UrlMappings.groovy
@@ -5,5 +5,8 @@ class UrlMappings {
 
     static mappings = {
         "/wsopen/alerts/user/$userId"(controller: 'webservice', action: [GET: 'getUserAlertsOutsideCAS'])
+        "/api/savedSearch/create"(controller: 'savedSearch', action: [POST: 'create'])
+        "/savedSearch/list"(controller: 'savedSearch', action: [GET: 'list'])
+
     }
 }
